f1f6ec46e533dc1a7ccc710e85bf11afd6c95e6e
Vyshnavi-Ravuri <vyshnavi.22bce9169@vitapstudent.ac.in>
2026-03-22 16:02:45 +0530

    updating server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const appointmentRoutes = require("./routes/appointmentRoutes");
@@ -24,6 +23,9 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 app.listen(PORT, () => {
     console.log(` Server running on port ${PORT}`);
