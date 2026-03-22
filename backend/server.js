fd22d5573c5279fea66b2febab0b185d9e204295
Vyshnavi-Ravuri <vyshnavi.22bce9169@vitapstudent.ac.in>
2026-03-22 18:56:54 +0530

    updating server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const appointmentRoutes = require("./routes/appointmentRoutes");
 const doctorRoutes = require("./routes/doctorRoutes");
 const cors = require("cors");
+
 dotenv.config();
 
 const app = express();
@@ -13,20 +14,21 @@ app.use(express.json());
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/doctor", doctorRoutes);
 
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected Successfully"))
-    .catch((err) => console.log(" DB Error:", err.message));
-
 app.get("/", (req, res) => {
     res.send("Doctor Appointment Backend Running ");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
-app.listen(PORT, () => {
-    console.log(` Server running on port ${PORT}`);
-});
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB Connected Successfully");
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("DB Error:", err.message);
+        process.exit(1);
+    });
